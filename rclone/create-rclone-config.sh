5bcf835b8eec420fd617c700a3be0a6984ddc990
slink42 <13059870+slink42@users.noreply.github.com>
2020-09-26 20:18:20 +1000

    bugfix cat vs echo

--- rclone/create-rclone-config.sh
+++ rclone/create-rclone-config.sh
@@ -8,7 +8,7 @@ RCLONE_CONFIG_DIR=${RCLONE_CONFIG%/*}
 mkdir -p $RCLONE_CONFIG_DIR
 
 if ! [ -z "${RCLONE_CONFIG_ENCRYPTED}" ]  && ( ! [ -z "${RCLONE_CONFIG_PASS}" ] || ! [ -z "${OP}" ]  )  ; then
-    cat $RCLONE_CONFIG_ENCRYPTED > $RCLONE_CONFIG
+    echo "$RCLONE_CONFIG_ENCRYPTED" > $RCLONE_CONFIG
     echo "note: RCLONE_CONFIG_ENCRYPTED env variable defined. Applying overwriting rclone config $RCLONE_CONFIG with encrypted config"
 fi
 
