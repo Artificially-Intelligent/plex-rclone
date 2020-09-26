099b59d866f8e835c57678c0bf96b1108ba070ee
slink42 <13059870+slink42@users.noreply.github.com>
2020-09-26 20:40:55 +1000

    fixed config encryption format

--- rclone/create-rclone-config.sh
+++ rclone/create-rclone-config.sh
@@ -7,11 +7,6 @@ fi
 RCLONE_CONFIG_DIR=${RCLONE_CONFIG%/*}
 mkdir -p $RCLONE_CONFIG_DIR
 
-if ! [ -z "${RCLONE_CONFIG_ENCRYPTED}" ]  && ( ! [ -z "${RCLONE_CONFIG_PASS}" ] || ! [ -z "${OP}" ]  )  ; then
-    echo "$RCLONE_CONFIG_ENCRYPTED" > $RCLONE_CONFIG
-    echo "note: RCLONE_CONFIG_ENCRYPTED env variable defined. Applying overwriting rclone config $RCLONE_CONFIG with encrypted config"
-fi
-
 if ! [ -z "$RCLONE_CONFIG_URL" ] ; then
     echo "RCLONE_CONFIG_URL defined. Attempting to download latest config file"
     curl -L -o ./rclone.conf $RCLONE_CONFIG_URL 
@@ -33,7 +28,17 @@ if [ ! -f "${RCLONE_CONFIG}" ]; then
     RCLONE_CONFIG_DIR=${RCLONE_CONFIG%/*}
     mkdir -p $RCLONE_CONFIG_DIR
     
-    cat << EOT > $RCLONE_CONFIG
+    if ! [ -z "${RCLONE_CONFIG_ENCRYPTED}" ]  && ( ! [ -z "${RCLONE_CONFIG_PASS}" ] || ! [ -z "${OP}" ]  )  ; then
+        cat << EOT > $RCLONE_CONFIG
+# Encrypted rclone configuration File
+
+RCLONE_ENCRYPT_V0:
+EOT
+        echo "$RCLONE_CONFIG_ENCRYPTED" >> $RCLONE_CONFIG
+        echo "note: RCLONE_CONFIG_ENCRYPTED env variable defined. Applying overwriting rclone config $RCLONE_CONFIG with encrypted config"
+    else
+
+        cat << EOT > $RCLONE_CONFIG
 [REMOTE]
 type = drive
 
@@ -44,15 +49,15 @@ remote = REMOTE:
 [PLEXDRIVE_CRYPT]
 type = crypt
 EOT
-    if [ -z "${PLEXDRIVE_MOUNT_CONTAINER_PATH}" ]; then
-        # duplicated from mount-plexdrive.sh as changes made there not accessible
-        PLEXDRIVE_MOUNT_CONTAINER_PATH=/mnt/plexdrive/
+        if [ -z "${PLEXDRIVE_MOUNT_CONTAINER_PATH}" ]; then
+            # duplicated from mount-plexdrive.sh as changes made there not accessible
+            PLEXDRIVE_MOUNT_CONTAINER_PATH=/mnt/plexdrive/
+        fi
+        echo "remote = $PLEXDRIVE_MOUNT_CONTAINER_PATH" >> $RCLONE_CONFIG
+        
+
+        echo "note: generic rclone config file $RCLONE_CONFIG contents:"
+        cat $RCLONE_CONFIG
+        echo "If you want to replace this with a different file, mount a volume containing a rclong.conf file and/or setup a new rclone.conf by running the command (replacing plex-rclone with your container name if different) ' docker exec -it plex-rclone rclone config --config $RCLONE_CONFIG ' Create a 'new remote' named $RCLONE_MOUNT_REMOTE_PATH  (without the : and any text following it), or add the name you choose followed by : to enviroment variable RCLONE_MOUNT_REMOTE_PATH"
     fi
-    echo "remote = $PLEXDRIVE_MOUNT_CONTAINER_PATH" >> $RCLONE_CONFIG
-    
-
-    echo "note: generic rclone config file $RCLONE_CONFIG contents:"
-    cat $RCLONE_CONFIG
-
-    echo "If you want to replace this with a differnt file, mount a volume containing a rclong.conf file and/or setup a new rclone.conf by running the command (replacing plex-rclone with your container name if different) ' docker exec -it plex-rclone rclone config --config $RCLONE_CONFIG ' Create a 'new remote' named $RCLONE_MOUNT_REMOTE_PATH  (without the : and any text following it), or add the name you choose followed by : to enviroment variable RCLONE_MOUNT_REMOTE_PATH"
 fi
