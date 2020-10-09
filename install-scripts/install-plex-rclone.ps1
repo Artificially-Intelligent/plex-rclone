9fcab204af90bcf7e4da43ea272bbe3c2774cace
slink42 <13059870+slink42@users.noreply.github.com>
2020-10-09 19:11:01 +1100

    diabled big buffer addition

--- install-scripts/install-plex-rclone.ps1
+++ install-scripts/install-plex-rclone.ps1
@@ -6,7 +6,13 @@
 )
 
 $LanInterface = Get-NetIPAddress -AddressFamily IPv4 | Where-Object -FilterScript { $_.ValidLifetime -Ne ([TimeSpan]::MaxValue) }
+$hostname = hostname
 Write-Output ("Discovered Local IPAddress: " + $LanInterface[0].IPv4Address)
+
+$host_subnet = "192.168.0.0/16,172.16.0.0/12,10.0.0.0/8"
+$host_subnet = ([IPAddress] (([IPAddress] $LanInterface[0].IPv4Address).Address -band ([IPAddress] "255.255.255.0").Address)).ToString() + "/32"
+
+
 
 
 function test-url($uri){
@@ -23,8 +29,8 @@ $dockercompose = ('
 version: "2.1"
 services:
   plex:
-    image: artificiallyintelligent/plex-rclone:test
-    container_name: plex
+    image: artificiallyintelligent/plex-rclone
+    container_name: plex-rclone
     devices:
       - "/dev/fuse:/dev/fuse"
     cap_add:
@@ -32,26 +38,18 @@ services:
     environment:
       - PUID=99
       - PGID=100
-      - VERSION=docker
+      - VERSION=latest
       - UMASK_SET=022 #optional
       - PLEX_CLAIM=REPLACE_ME #optional
-      - ADVERTISE_IP="http://' + $LanInterface[0].IPv4Address + ':' + $plex_port.ToString() + '"
       # - RCLONE_SERVE_PORT=13670
       - RCLONE_GUI=TRUE
-      - "RCLONE_DRIVE_TEAM_DRIVE=0ALNBa0QBHhldUk9PVA"
-      - "RCLONE_DRIVE_SCOPE=drive.readonly"
-      - ''RCLONE_DRIVE_SERVICE_ACCOUNT_CREDENTIALS={"type": "service_account","project_id": "rclone-280007","private_key_id": "2fa244747e21efcecd31727203c0abbe1a6c10f9","private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC3EvvM8GbYejVR\nS0R2+qxdgcNOrc69fBiUOANKNtj/ry1tHm9DrtmctlcB/Va4QmQ000dPT7xB+zTH\nGIOUvsCJXJAntXmYDJyGWlMhRNcR/InvFJ0prWuwjKMxx6vijNpb5BRBXNEJVWxi\nl1CLdyur+IUlWIxV0jcsgl7OeTCpsA8Q5zua2GPamay7AUzSZs5/n4/azhbPRH2b\nMTvpzLEEIqVDFGJMQ5EOr0fiIb8Gq/6tYdMkdU15xREzQ2MUvDh/RsXhss5+J8UO\nU5BTNEFFoqTXBy+T8a+dipRGXrhIlFh5V1yB2uoShglv3OW/nyzsEA28D7+TIj2T\nGWNVgTARAgMBAAECggEAAWl+kraZ/Hp41c8wUICferso//7cNN7zq5UBBX4Fz3cQ\n8SIGdq2vFQPdCPFkzVgJwNSOXMC/MX0YC58XSLrt7kFOBVhjfzY9sNWahYur4wIh\nKDxu9+bUCVEUYypom389oe7Q7y4hmKJViy8immjJZ/KrSD32GyVbcpbw10PdCboE\nELfIa026HgAc6mhtMIuVDa1IKV5P+adBHlFpnIiivYbaJS03y0Y27AzMTdikUog4\nDrwQOQjb+J8Yvtvh0DicyHiWpoo3PblEbNVD3po5jU5HRqYyGr+C4/aGE9VoX63y\nSFPvy/p0vDZLHOXijwxmnp6z8chfnuWUQp7FDL26AQKBgQDuogJcBHLPgZw8Yd6Q\nXUyw2ROCkIDlAqynRr05EgBHxqGR0kxeG+VbJR58ST0+pcLXws1/qAgKgwtjRSQX\nIfDMcc1vXBTmLMzp5e5O8gCId0a+I0WehfFxRlvwFu70wWGbaMcWB5KJiAU9g28p\nIbgDUqHHrf8SuoBAtq7uT3gfUQKBgQDEZdrtuUQx/DJnOM3cH6GJIrSl5w34VjnB\nyGjnxrU60ddVxKom4h7OuFSap1pqAe8vCu38VKFrvbe4lnICt5R47J8BYHZDgXYg\nvbIhSdvHOU0sX3Mx39t1DEYreWH8NDwgcGu/z3zwB1LM1ugPtHWjIcAHhKlpm4G/\n/RC7qjtUwQKBgQDbXvXaT4CX/+d3nuTyQ8LXpIcJ9Pt5C89aa25SQ5kcYp5vJits\nLCrZcjMnQFDcAZgvrvYpD3hs6XETjiESXvI1j7yyTa0suCycLPK5gkE5MqVG77f1\ndd6yKmMkQIDlYczwCA9U0htE8VUX6nbLEiNOcq0kmnCtb+OtGgOQMmAUMQKBgQCe\nGjjR4rDpZFLXEb13FOefVBcE7yop3pAEVedNnoNKZJ7q5rTrNGEEnRNOpKZ+bCw2\nPKA035RK/aEmscX6NfsKFcIzA4pw8Yk89jit351Tledwby522fT7FvRuDvs4Ynx8\nyMOU192GBP8880xBSE7jEkpaQwt2fr9G5NxRlsrIgQKBgGl0F3HLylgimI3/WIvU\nQC/oaQAIOkJsZtZ34GWgUakuSDA2IATR3czQgTsV0yjZTzOo2agix3uYZ5QhUswq\nvhv/9WzhfACJc9BRedsblJusbwbX4UpXthswdTtA7AyuPqHALzhKn7xbpqCouPPs\nwOHyC5qCzJn+nE+HHJQ3TLrm\n-----END PRIVATE KEY-----\n","client_email": "plex-rclone-container@rclone-280007.iam.gserviceaccount.com",  "client_id": "106557646939466827684",  "auth_uri": "https://accounts.google.com/o/oauth2/auth",  "token_uri": "https://oauth2.googleapis.com/token",  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/plex-rclone-container%40rclone-280007.iam.gserviceaccount.com"}''
-      - "RCLONE_CRYPT_PASSWORD=j3UvpVE7TxqAscDp7O-28-bzmKdgENxCMdqrwb8aQ9vzqLx7f4JIGtSMhzTjRNmHM_oHYqQoad61jr3m6TV9L3gceb_LBMFGX1X4I5IkrPxYQgE-pglSZRfkiGO2LUYsu3X7myid0-lMBGcjPdEIYBEzkyRFGB3q3woDnYdDWrQY7yzJuMMPwB_X5jwCPC-p2ad-YyyxhHggrDLhfbieePvotjTeGY9S9ue-uWzUSl6d19Kji7er-iUJag"
-      - "RCLONE_CRYPT_PASSWORD2=JhaXkC7UOC_ecNAP2kNmHZ4rq1nmQ6XyGzh02v_Au8GpnJXEg_5KTONa8lXrezUVgLn9nrLrLrNjZ7OdeYXt1vmAKKHuUGBVrkyD_iz3YWre5AXknmTuyE0dCYNe2CbMrmqDYROV6DkvrCpdEAQ2620U1iZFJVWNjGasCtWh-Al3sw5poQ2cCdEYasV1Eq-6k4wOPUQBGGpTBEFU0USfaQCGoJ9GUZ6eWoqXg-1DcU-_Y9sNSyU4NRz8pA"
-      - "RCLONE_CRYPT_DIRECTORY_NAME_ENCRYPTION=true"
-      - "RCLONE_CRYPT_FILENAME_ENCRYPTION=standard"
-      - "PLEX_LIBRARY_MASTER_PATH=/mnt/rclone/Storage/plex-library-v2.tar.gz"
-      - ''PLEXDRIVE_CONFIG_JSON={"ClientID":"417005134584-olp2v0h5ffb396kdthin4shfj9vsg1fn.apps.googleusercontent.com","ClientSecret":"2D1QAHVtaez4BFqiGEd020fB"}''
-      - ''PLEXDRIVE_TOKEN_JSON={"access_token":"ya29.a0AfH6SMA9vXOdWpzv3HQTcXer10tJaudGaoUsZULwyuTtWVkW95AVJ7GeNUiwM6B_IMclgbl9NSBcKe8QyrW_NQx1CTPFW8I51DTVazfGtIy5RKI57p8CZ6t0sUYGh_FS7IvIg9A_lC8dNyyffzrOqrgkFfHD6W1W3hE","token_type":"Bearer","refresh_token":"1//0gczToZ9i0dzGCgYIARAAGBASNwF-L9IrgTF92rq_uPzzCm7L-MDzqlxPDFcpJxqJkubs9XByKumsQ1tdwghWBAMQUj4Wj3IMM_k","expiry":"2020-06-25T19:36:54.398361227+10:00"}''
+      - "RCLONE_CONFIG_ENCRYPTED=dHRbzZq6aJhoazoqL5ha36LnoznBK0nO0OQqdcXtfoTXG6cLwww+WGWotGMan3E+F2ckH/7mVVe6jMsernNA+tZx7RXHL0NQ8xYuMp2UAimL+2Na6UavSQs4yT+TaZB+gRsKczHXN9wYPZoSAJbkp0cSFk4uTvq/lEdV97vn4c77HDJPMu2Z5R8zPWCt48O1S/S0cxcAukpNiRPRovKVbGL8I3BkLluAlTcSYUUdccZSFsZlw4M7NhNKyi3jdg7fs1qJKPsH1R4ZYiB7WXKjoWU4UrwnHEQ60VA5QVqLLgSHvolH/h9fWDl2ZIuDF1xU6UN5PhH03z68WB3TUbT5XJWBSMbPR+bH5HhQRfCwXll5P+GOzFME0fEt6m+AT8/YpTU/yX5SsRVXWcl1OU6V5GJGAKBjFpp3HX6dNjjSHtxLwyeYqHWpZjBp0g8Hg8j2Lm8+5qV1nGqxd5AJjMaWWNlSKjWqAi7vL8OpUuRWTvEI2TH/ZnKZVkQjdHa4vm02KSqxDqVJ6IwJXMeBuHs+PXo8rMk2RR13fTb/2M1JaRc3zxEG2I33cmHCQxKbcBGwhB7q0lNnK9ibA0WOe/pBIoytlMGGVjcTMXMgDTeOWguTItIlnqi4fYkS+nsX4XH5VB7X3GpvIZxZuoeIfYlKwDiVqocoDjg/kessBQgI7NLLx8c7lfhhLjJoA06EDb3/ZGcYn1B0KKHFRo3B/RupvCEH/hoV8xOXxTsWPwz8kBRZ/6yiLV7t6Rzd41wjfSuOvEjAX50WPIopn2EIDEfAeQZhEkoGXW1MgiWidN23YSEFpMpzrtZm+W30WymNYn0ELeVGTIDvwotXIq5DvZkULr5IopBZNzxdtrUgByjXcrGh6M0m78DCCIcmRIlS5Ek4REsS3jrBUF/jhuMbc/nd5pE/su9VR3v/soXkU+fDRx8V4adgDpN3OlwmGUJIc3sjaUJTKMDKbomMNyp1YQF8wsP2ix3FQP0MS5oNe6SXi6UYDwWevvmXUHc7M1E0d2bmWNk3hDYxCr9aiqV7X5Z97HRT7rU8wXFf5jUgcG/uOgKHrTVDFyktvS2ET6XhOPQgObYJE9vZx3TDu/io1dvKYzNImnachrDLAUbYcZlypNXx8mwPjKySPMy9jzbZPNZZE9JOTnpQqJs7XVgkzmW1bJKCVxkzWF8yvt/S3NgGPTvO5WEDVZ/HfNuN9mTapcaCsZ70nhtxOkYshbFKrXOAG2ARrtdxNJuPD0LXE1Ly67M4GUxvr1+q49VzMe6E0ceCOEowbR9lK3to1dAqcRbE7jbMykf5gIW+2KY4xwsNuI0Uxdfrj31dK1puP7V4bX0IAAuPTNxp6P5RfHBH/JK5buBjxfqubkQg2jwQy5/nzbmq6DtM2DBQ05xVdY06IFJmsfW4hIJG1QV0F52Znoje9BAN7zthcj8RS6dmlUFQ3CNusFXP2FOxQMy/Vn+oQT5zCv2xb+XdVHjeRkqr9cqxTvMnJi+h+c6r0P++36SCD8ujAN7KOCCn9qtNYsh/hcqFh1DSCWp8iE373RDRh/5PYpWP0ppBuMwnrnfvazn52eM16J7V/0KsCPzkyARvf7rmdLmp+HvdIk4/GtyG8vAHnn3ab8QaLXXJee9Bv9wk5gXX+4wllxR9uJtrJahkhIPwy7yJGyyyITpUFMqc5odyYH0E0VAcge9hmiz1QtKNG/Y6Vjg+ToxtZmOfF9rKPXNN5B9dXN+v0isL44dCN3WAjA3jlCLgUdv0qB1rNjZ5/v2XYyLKlCQEg5wdR3m1cfb747KvcwIk1ifqd5rWYEdV0bas7Eelh8YDRWnKL0g9n1e9BQK8UyezhqFWjBnq3UEP+ciefiEaaiPuEafOkUHMQVFkVylKGXQSHOo93ohY8lv6WPBKV1MU0yqL+uK4uHpVTYswGVyBJRhoJwoe0UKx+9kwDeCE+/C2REPtJnK9kpi7USgQcV8IY5BA8Lv2fmAgjVe+qW1fTz6zyzpExNXYqQuJhUpI2/Wbsnvj5sfz0euKQvuolxREbAZ6jjKSOONg/Q=="
+      - "PLEX_LIBRARY_MASTER_PATH=/mnt/rclone/Storage/Backup/plex-rclone/plex-library.tar.gz"
       - PLEXDRIVE=TRUE
-      - FriendlyName=plex-rclone
-      - allowedNetworks=192.168.0.0/16,172.16.0.0/12,10.0.0.0/8
-      - LanNetworksBandwidth=192.168.0.0/16,172.16.0.0/12,10.0.0.0/8
+      - FriendlyName=' + $hostname + '-docker
+      - ADVERTISE_IP="http://' + $LanInterface[0].IPv4Address + ':' + $plex_port.ToString() + '" # this should be the ip address of the host computer
+      - allowedNetworks='+ $host_subnet + '
+      - LanNetworksBandwidth='+ $host_subnet + '
       - TreatWanIpAsLocal=1
       - DlnaEnabled=1
       - autoEmptyTrash=0
@@ -66,20 +64,20 @@ services:
       - ScheduledLibraryUpdatesEnabled=1
       - TranscoderTempDirectory=/transcode
       - RelayEnabled=0
+      - OP=s0Qib5trYnlxujiTSIyCax2UhYQj3NHvJJazeg
+      - CUSTOM_PLEX_PROFILES=chromecast
     volumes:
       - plex_temp:/transcode
       - plex_config:/config
     ports:
-      - "1900:1900/udp"
       - "3005:3005"
-      - "5353:5353/udp"
       - "8384:8384"
       - "13668-13670:13668-13670"
-      - "32410-32414:32410-32414/udp"
       - "32469:32469"
       - "' + $plex_port.ToString() + ':32400"
     restart: unless-stopped
   watchtower:
+    container_name: watchtower
     image: containrrr/watchtower
     volumes:
       - /var/run/docker.sock:/var/run/docker.sock
@@ -88,16 +86,17 @@ services:
 volumes:
   plex_temp:
   plex_config:
+
 ')
 
 # Check if plex is active yet.
 $plex_url = 'http://localhost:' + $plex_port.ToString() + '/web/index.html'
 
 
-if( (test-url($plex_url )) -eq 200){
-    Write-Error "Plex Media Server Port allready allocated, try running again with flag: -plex_port 12345"
-    Exit 1
-}
+#if( (test-url($plex_url )) -eq 200){
+#    Write-Error "Plex Media Server Port allready allocated, try running again with flag: -plex_port 12345"
+#    Exit 1
+#}
 
 $quiet = mkdir -f  $programfolder
 
@@ -105,12 +104,21 @@ $dockercompose_filename = 'docker-compose.yaml'
 $dockercompose_filepath = (Join-Path -Path $programfolder -ChildPath $dockercompose_filename)
 
 Out-File -FilePath $dockercompose_filepath -InputObject $dockercompose
+Set-Location $programfolder
+docker-compose.exe pull
 docker-compose.exe -f ($dockercompose_filepath) up -d
+
+$docker_auth = 'docker.exe exec -it plex-rclone authenticate'
+Write-Output $docker_auth
+cmd /c  $docker_auth
+
+docker-compose.exe restart
 
 # We then get a response from the site.
 while((test-url($plex_url )) -ne 200){
     Write-Output ("Plex library download and install not complete yet. Sleeping " + $delay.ToString() + " min then checking again.")
-    sleep ($delay * 60)
+    Start-Sleep ($delay * 60)
 }
 
-Start $plex_url
+Start-Process $plex_url
+
